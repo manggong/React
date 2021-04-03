309cdf4aa53153513d574556e506a1d71197d081
manggong <jihwank1995@gmail.com>
2021-04-03 12:34:45 +0900

    add TodoTemplate

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,11 +1,8 @@
+import TodoTemplate from './components/TodoTemplate';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
+  return <TodoTemplate>Todo App을 만들자~!</TodoTemplate>;
 }
 
 export default App;
