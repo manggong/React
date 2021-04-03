dc7fe3d4afbd98e02f8bd743b44f678632dc3fae
manggong <jihwank1995@gmail.com>
2021-04-03 11:40:18 +0900

    add scss

---
+++ style_practice/src/App.js
@@ -0,0 +1,12 @@
+import SassComponent from './SassComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SassComponent/>
+    </div>
+  );
+}
+
+export default App;
