42225c5df563f594e3cd7f059b0b542d3d5bd989
manggong <jihwank1995@gmail.com>
2021-04-04 23:09:55 +0900

    add insert function

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { MdAdd } from 'react-icons/md';
 import '../styles/TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
-      <input placeholder="Insert To-do" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input placeholder="Insert To-do" value={value} onChange={onChange} />
       <button type="submit">
         <MdAdd />
       </button>
