e5b90635249d9f24286c6944ae7141f5824c4fc4
manggong <jihwank1995@gmail.com>
2021-04-04 22:07:07 +0900

    add dummy datas in App

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,32 @@
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'This is first text',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'This is second text',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'This is third text',
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 }
