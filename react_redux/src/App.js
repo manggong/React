96bccca9fdc48ab8fae3ee7d9db2d2880491ab27
manggong <jihwank1995@gmail.com>
2021-04-18 21:06:04 +0900

    add Todos

--- react_redux/src/App.js
+++ react_redux/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
+import Todos from './components/Todos';
 
 function App() {
   return (
     <div className='App'>
       <Counter number={0} />
+      <hr />
+      <Todos />
     </div>
   );
 }
