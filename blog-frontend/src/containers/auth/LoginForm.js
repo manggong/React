f557d789ab34e04d5ff2378074a151eaa15ba37b
manggong <jihwank1995@gmail.com>
2021-04-19 18:55:20 +0900

    combine LoginForm container, LoginForm component

--- blog-frontend/src/containers/auth/LoginForm.js
+++ blog-frontend/src/containers/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
