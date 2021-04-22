d237f62a182e79c0e706e157945555c6c0f5715e
manggong <jihwank1995@gmail.com>
2021-04-22 20:51:08 +0900

    add filter, fetch data in Grid

--- antd-aggrid-app/src/components/Grid.js
+++ antd-aggrid-app/src/components/Grid.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
@@ -9,17 +9,31 @@ const Grid = () => {
   const [gridColumnApi, setGridColumnApi] = useState(null);
 
   const [rowData, setRowData] = useState([
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
+    // { make: 'Toyota', model: 'Celica', price: 35000 },
+    // { make: 'Ford', model: 'Mondeo', price: 32000 },
+    // { make: 'Porsche', model: 'Boxter', price: 72000 },
   ]);
+
+  useEffect(() => {
+    fetch('https://www.ag-grid.com/example-assets/row-data.json')
+      .then((result) => result.json())
+      .then((rowData) => setRowData(rowData));
+  }, []);
 
   return (
     <div className='ag-theme-alpine' style={{ height: 400, width: 600 }}>
       <AgGridReact rowData={rowData}>
-        <AgGridColumn field='make'></AgGridColumn>
-        <AgGridColumn field='model'></AgGridColumn>
-        <AgGridColumn field='price'></AgGridColumn>
+        <AgGridColumn field='make' sortable={true} filter={true}></AgGridColumn>
+        <AgGridColumn
+          field='model'
+          sortable={true}
+          filter={true}
+        ></AgGridColumn>
+        <AgGridColumn
+          field='price'
+          sortable={true}
+          filter={true}
+        ></AgGridColumn>
       </AgGridReact>
     </div>
   );
