0dfc78e0318161c136c93608fa9e17c81f37aedf
manggong <jihwank1995@gmail.com>
2021-04-22 21:10:52 +0900

    add DatePicker

--- antd-aggrid-app/src/components/Antd.js
+++ antd-aggrid-app/src/components/Antd.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, DatePicker, message } from 'antd';
+import 'antd/dist/antd.css';
 
 const Antd = () => {
+  const [date, setDate] = useState(null);
+
+  const handleChange = (value) => {
+    message.info(
+      `Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`,
+    );
+    setDate(value);
+  };
   return (
-    <div>
-      <h1>ant Design page</h1>
+    <div style={{ width: 400, margin: '100px auto' }}>
+      <DatePicker onChange={handleChange} />
+      <div style={{ marginTop: 16 }}>
+        Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
+      </div>
+      <Button type='primary' style={{ marginLeft: 8 }}>
+        Primary Button
+      </Button>
     </div>
   );
 };
