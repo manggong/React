0f07336a58bf2063edebd1076750ad74f9089be1
manggong <jihwank1995@gmail.com>
2021-04-25 17:11:43 +0900

    add redux-saga, login, register redux

--- blog-frontend/src/lib/createRequestSaga.js
+++ blog-frontend/src/lib/createRequestSaga.js
@@ -1,10 +1,14 @@
 import { call, put } from 'redux-saga/effects';
 import { startLoading, finishLoading } from '../modules/loading';
 
-export default function createRequestSaga(type, request) {
+// prefix 인자로 받아서 액션 타입 돌려쓰기
+export const createRequestActionTypes = (type) => {
   const SUCCESS = `${type}_SUCCESS`;
   const FAILURE = `${type}_FAILURE`;
+  return [type, SUCCESS, FAILURE];
+};
 
+export default function createRequestSaga(type, request) {
   return function* (action) {
     yield put(startLoading(type));
     try {
